d9be663baae28ee07b63056dbe962d1ba836950d
Brennedith Garcia <brennedithg@gmail.com>
2019-06-10 13:50:30 -0500

    Correct paths

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// CREATE
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// READ
+// UPDATE
+// DELETE
 
 module.exports = router;
