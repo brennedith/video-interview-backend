41244db00904a67a79554044ddb23cb8d95b35c8
Brennedith Garcia <brennedithg@gmail.com>
2019-06-24 09:58:36 -0500

    Add ffmpeg conversion library

--- routes/video.js
+++ routes/video.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const ffmpeg = require('fluent-ffmpeg');
 const ftp = require('ftp');
 
 const router = express.Router();
@@ -12,24 +13,31 @@ router.post('/', upload.single('video'), (req, res, next) => {
   const { accountKey, host, user, password } = req.body;
 
   const fileLocation = req.file.path;
+  const fileLocationMp4 = `${fileLocation}.mp4`;
   const fileName = `${accountKey}.mp4`;
 
-  ftpClient.on('ready', () => {
-    ftpClient.put(fileLocation, fileName, err => {
-      if (err) return res.status(500).json({ message: err.message });
+  ffmpeg(fileLocation)
+    .inputFormat('webm')
+    .outputOptions('-qscale 0')
+    .save(fileLocationMp4)
+    .on('end', function() {
+      ftpClient.on('ready', () => {
+        ftpClient.put(fileLocationMp4, fileName, err => {
+          if (err) return res.status(500).json({ message: err.message });
 
-      res.status(200).json({ message: 'OK' });
-      ftpClient.logout(() => {});
+          res.status(200).json({ message: 'OK' });
+          ftpClient.logout(() => {});
+        });
+      });
+
+      ftpClient.connect({
+        host,
+        user,
+        password,
+        port: 21,
+        secure: false
+      });
     });
-  });
-
-  ftpClient.connect({
-    host,
-    user,
-    password,
-    port: 21,
-    secure: false
-  });
 });
 
 module.exports = router;
